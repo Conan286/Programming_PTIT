1b3b6969b46e8b54bb5d374d1274455e3de03634
quanghuy0411 <92586415+quanghuy0411@users.noreply.github.com>
2022-11-02 20:30:30 +0700

    Create BÌNH PHƯƠNG.cpp

---
+++ SAMSUNG_ALGORITHMS/BASIC/BÌNH PHƯƠNG.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#define ll long long
+#define cin(n) scanf("%lld",&n);
+#define cout(n) printf("%lld\n",n);
+int main()
+{
+int t;
+cin(t)
+while(t--){
+ll n;
+cin(n)
+cout(n*n)
+}
+}
