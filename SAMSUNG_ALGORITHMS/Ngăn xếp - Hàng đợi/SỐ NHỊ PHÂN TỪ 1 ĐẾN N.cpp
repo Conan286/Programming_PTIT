f9ed4e9e67cdbb39f19e7bb47ba89cae4d4833af
quanghuy0411 <92586415+quanghuy0411@users.noreply.github.com>
2022-11-02 21:09:55 +0700

    Create SỐ NHỊ PHÂN TỪ 1 ĐẾN N.cpp

---
+++ SAMSUNG_ALGORITHMS/Ngăn xếp - Hàng đợi/SỐ NHỊ PHÂN TỪ 1 ĐẾN N.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<cstring>
+#include<cmath>
+#include<map>
+#include<vector>
+#include<set>
+#include<stack>
+#include<queue>
+#include <algorithm>
+#include<sstream>
+#include<iomanip>
+#define ll long long
+
+#define yes puts("YES");
+#define no puts("NO");
+#define k 1000000007
+using namespace std;
+
+string binary(int n)
+{
+string s="";
+stack<int>st;
+while(n)
+{
+st.push(n%2);
+n=n>>1;
+}
+while(!st.empty()){
+ s+=to_string(st.top());
+ st.pop();
+}
+return s;
+}
+int main()
+{
+    int t,n;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        for(int i=1;i<=n;i++)
+        cout<<binary(i)<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
