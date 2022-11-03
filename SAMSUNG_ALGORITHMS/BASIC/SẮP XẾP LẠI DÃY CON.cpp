05bd67bef2452391f66d38d9dc9df090785b192c
quanghuy0411 <92586415+quanghuy0411@users.noreply.github.com>
2022-11-03 21:05:19 +0700

    Create SẮP XẾP LẠI DÃY CON.cpp

---
+++ SAMSUNG_ALGORITHMS/BASIC/SẮP XẾP LẠI DÃY CON.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve(int a[],int b[],int n)
+{
+int l=0,r=n-1;
+while(a[l]==b[l])++l;
+while(a[r]==b[r])--r;
+cout<<l+1<<" "<<r+1<<endl;
+}
+main()
+{
+int t,n,x=0;
+cin>>t;
+while(t--)
+{
+cin>>n;
+int a[n],b[n];
+for(int i=0;i<n;i++)
+{
+cin>>a[i];
+b[i]=a[i];
+}
+sort(a,a+n);
+solve(a,b,n);
+}
+}
